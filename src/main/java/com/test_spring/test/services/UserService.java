405d52998a7de022ab2bb1058cdb35a098c68797
Leonardo Basseto <leobasseto0910@gmail.com>
2026-03-02 12:29:32 -0300

    Exception handling - update

--- src/main/java/com/test_spring/test/services/UserService.java
+++ src/main/java/com/test_spring/test/services/UserService.java
@@ -13,6 +13,8 @@ import com.test_spring.test.repositories.UserRepository;
 import com.test_spring.test.services.exceptions.DatabaseException;
 import com.test_spring.test.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -41,9 +43,13 @@ public class UserService {
 		}
 	}
 	public User update (Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);	
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
