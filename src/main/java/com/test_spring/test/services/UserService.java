db0551406920f2aecad430bdd2a63410d54e7722
Leonardo Basseto <leobasseto0910@gmail.com>
2026-03-02 12:20:45 -0300

    Exception handling - delete

--- src/main/java/com/test_spring/test/services/UserService.java
+++ src/main/java/com/test_spring/test/services/UserService.java
@@ -4,11 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.test_spring.test.entities.User;
 import com.test_spring.test.repositories.UserRepository;
+import com.test_spring.test.services.exceptions.DatabaseException;
 import com.test_spring.test.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +32,13 @@ public class UserService {
 		return repository.save(obj);
 	}
 	public void delete(Long id) {
+		try {
 		repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	public User update (Long id, User obj) {
 		User entity = repository.getReferenceById(id);
