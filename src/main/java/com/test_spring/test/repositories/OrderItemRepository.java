c220eb009e68907a6875e601cc2c252626e55813
Leonardo Basseto <leobasseto0910@gmail.com>
2026-03-10 15:14:50 -0300

    Update and rename package to EcommerceApi

--- src/main/java/com/test_spring/test/repositories/OrderItemRepository.java
+++ src/main/java/com/test_spring/test/repositories/OrderItemRepository.java
@@ -1,8 +1,8 @@
 package com.test_spring.test.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.test_spring.test.entities.OrderItem;
+import com.test_spring.test.entities.pk.OrderItemPK;
 
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPK> {
 }
